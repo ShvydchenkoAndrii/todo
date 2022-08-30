5dc270b98dc96c49986cd9c693d982568c348afc
unknown <shvydchenkoandriy@gmail.com>
2022-08-31 00:23:01 +0300

    try to add selectorAll for buttons

--- js/index.js
+++ js/index.js
@@ -1,20 +1,29 @@
 (function () {
-    const button1 = document.querySelector('.butt1')
-    const button2 = document.querySelector('.butt2')
-    const button3 = document.querySelector('.butt3')
-    // const card = document.getElementById('card')
+    // const button1 = document.querySelector('.butt1')
+    // const button2 = document.querySelector('.butt2')
+    // const button3 = document.querySelector('.butt3')
 
-    button1.addEventListener('click', () => {
-        const all = button1.closest('.card')
-        all.remove();
-    })
-    button2.addEventListener('click', () => {
-        const all = button2.closest('.card')
-        all.remove();
-    })
-    button3.addEventListener('click', () => {
-        const all = button3.closest('.card')
-        all.remove();
-    })
+    // button1.addEventListener('click', () => {
+
+    //     const all = button1.closest('.card')
+    //     all.remove();
+    // })
+    // button2.addEventListener('click', () => {
+    //     const all = button2.closest('.card')
+    //     all.remove();
+    // })
+    // button3.addEventListener('click', () => {
+    //     const all = button3.closest('.card')
+    //     all.remove();
+    // })
+
+    const allButt = document.querySelectorAll('#card button .butt1')
+
+    for (let element of allButt) {
+        allButt.addEventListener('click', () => {
+            const all = element.closest('.card')
+            all.remove();
+        })
+    }
 
 })();

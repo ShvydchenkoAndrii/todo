ee8d836e702d98e6001d5c216a0bdd114d34e958
unknown <shvydchenkoandriy@gmail.com>
2022-08-30 22:08:28 +0300

    change js with removing html with func closest

--- js/index.js
+++ js/index.js
@@ -2,29 +2,19 @@
     const button1 = document.querySelector('.butt1')
     const button2 = document.querySelector('.butt2')
     const button3 = document.querySelector('.butt3')
-    const card1 = document.querySelector('.card1')
-    const card2 = document.querySelector('.card2')
-    const card3 = document.querySelector('.card3')
+    // const card = document.getElementById('card')
 
     button1.addEventListener('click', () => {
-        if (card1.style.display === 'none') {
-            card1.style.display = 'block'
-        } else {
-            card1.style.display = 'none'
-        }
+        const all = button1.closest('.card')
+        all.remove();
     })
     button2.addEventListener('click', () => {
-        if (card2.style.display === 'none') {
-            card2.style.display = 'block'
-        } else {
-            card2.style.display = 'none'
-        }
+        const all = button2.closest('.card')
+        all.remove();
     })
     button3.addEventListener('click', () => {
-        if (card3.style.display === 'none') {
-            card3.style.display = 'block'
-        } else {
-            card3.style.display = 'none'
-        }
+        const all = button3.closest('.card')
+        all.remove();
     })
+
 })();

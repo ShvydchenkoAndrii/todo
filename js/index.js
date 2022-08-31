9420ad348c97ef512b4d0d783ead77287ad096fc
unknown <shvydchenkoandriy@gmail.com>
2022-08-31 13:25:31 +0300

    add normal js )))

--- js/index.js
+++ js/index.js
@@ -1,27 +1,9 @@
 (function () {
-    // const button1 = document.querySelector('.butt1')
-    // const button2 = document.querySelector('.butt2')
-    // const button3 = document.querySelector('.butt3')
+    const button = document.querySelectorAll('.butt1')
 
-    // button1.addEventListener('click', () => {
-
-    //     const all = button1.closest('.card')
-    //     all.remove();
-    // })
-    // button2.addEventListener('click', () => {
-    //     const all = button2.closest('.card')
-    //     all.remove();
-    // })
-    // button3.addEventListener('click', () => {
-    //     const all = button3.closest('.card')
-    //     all.remove();
-    // })
-
-    const allButt = document.querySelectorAll('#card button .butt1')
-
-    for (let element of allButt) {
-        allButt.addEventListener('click', () => {
-            const all = element.closest('.card')
+    for (let element of button) {
+        element.addEventListener('click', () => {
+            let all = element.closest('.card')
             all.remove();
         })
     }

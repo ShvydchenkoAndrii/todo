066d2b4feae707fd4d741ab655efbc00c2b2080f
unknown <shvydchenkoandriy@gmail.com>
2022-09-01 18:49:41 +0300

    play with inputs

--- js/index.js
+++ js/index.js
@@ -22,16 +22,19 @@
 
     addButt.addEventListener('click', () => {
         const input = document.getElementById('myInput').value;
+        const input1 = document.getElementById('myInput1').value;
+        const input2 = document.getElementById('myInput2').value;
+        const input3 = document.getElementById('myInput3').value;
 
-        if (input !== '') {
+        if (input !== '' && input1 !== '' && input2 !== '' && input3 !== '') {
             let newElement = document.createElement('div');
             newElement.innerHTML = `
                 <div class="card" id="card">
                         <h2 class="first_name">${input}</h2>
                          <ul>
-                            <li>Last name: ${input}</li>
-                            <li>Age: ${input}</li>
-                            <li>Sex: ${input}</li>
+                            <li>Last name: ${input1}</li>
+                            <li>Age: ${input2}</li>
+                            <li>Sex: ${input3}</li>
                         </ul>
                         <input type="checkbox" class="checkbox">
                         <button class="butt1" id="butt">Hide</button>

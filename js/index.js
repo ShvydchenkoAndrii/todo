8c79498838d8f65f1abf006afa10f95eb2459e37
unknown <shvydchenkoandriy@gmail.com>
2022-09-04 17:45:04 +0300

    when click or enter input become clear

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 (function () {
     const cards = document.getElementById("todo_items_list");
     const addButt = document.querySelector(".add_todo");
+    const txtInput = document.querySelector('.what_todo');
 
-    addButt.addEventListener("click", () => {
-        const input = document.getElementById("myInput").value;
-
-        if (input !== "") {
+    addButt.addEventListener('click', () => {
+        let input = document.getElementById("myInput").value;
+        if (input !== '') {
+            document.getElementById("myInput").value = '';
             let newElement = document.createElement("div");
             newElement.setAttribute("class", "todo_item");
             newElement.innerHTML = `
@@ -17,7 +18,38 @@
             newElement.addEventListener("change", () => {
                 const name2 = newElement.closest(".todo_item").querySelector(".txt");
                 name2.classList.toggle("active");
+            })
+            const button = document.querySelectorAll(".butt1");
+
+        for (let element of button) {
+            element.addEventListener("click", () => {
+                let all = element.closest("div");
+                all.remove();
             });
+        }
+        }
+    })
+
+    txtInput.addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+            let input = document.getElementById("myInput").value;
+            if (input !== '') {
+                document.getElementById("myInput").value = '';
+                let newElement = document.createElement("div");
+                newElement.setAttribute("class", "todo_item");
+                newElement.innerHTML = `
+                          <input type="checkbox" class="checkbox">
+                          <h2 class="txt">${input}</h2>
+                          <button class="butt1" id="butt">delete</button>
+                   `;
+                cards.appendChild(newElement);
+
+                newElement.addEventListener("change", () => {
+                    const name2 = newElement.closest(".todo_item").querySelector(".txt");
+                    name2.classList.toggle("active");
+                });
+            }
+
         }
         const button = document.querySelectorAll(".butt1");
 
@@ -28,4 +60,5 @@
             });
         }
     });
+
 })();

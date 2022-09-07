163386ea5d2d14791266a5dd9cc6ea6d3a3ddca7
unknown <shvydchenkoandriy@gmail.com>
2022-09-07 15:06:27 +0300

    every button counter works, exept completed

--- js/index.js
+++ js/index.js
@@ -26,10 +26,15 @@
       newElement.addEventListener("change", () => {
         const name2 = newElement.closest("div").querySelector(".txt");
         name2.classList.toggle("active");
-        if (active === document.querySelector(".active_todo_items.checked")){
-          newElement.classList.add('hide')
-          count--;
+        count--;
         document.getElementById("counter").innerHTML = count;
+        if (active === document.querySelector(".active_todo_items.checked")) {
+          newElement.classList.add("hide");
+        }
+        if (
+          completed === document.querySelector(".completed_todo_items.checked")
+        ) {
+          newElement.classList.add("hide");
         }
       });
 

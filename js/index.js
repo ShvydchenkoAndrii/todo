29c1404b3d36b4d8d747e93b5d86802cbfa5c769
unknown <shvydchenkoandriy@gmail.com>
2022-09-29 14:06:45 +0300

    add filters to localStorage

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@
   const active = document.querySelector(".active_todo_items");
   const completed = document.querySelector(".completed_todo_items");
   const clear = document.querySelector(".clear_todo_items");
-  const newArr = [];
+  const newObj = {};
   let count = 0;
 
   function createNewToDo() {
@@ -101,6 +101,12 @@
         card.classList.remove("hide");
       }
     }
+    const allClass = all.classList;
+    const allStore = [];
+    allStore.push(allClass);
+    localStorage.removeItem("active");
+    localStorage.removeItem("completed");
+    localStorage.setItem("all", JSON.stringify(allStore));
   });
 
   active.addEventListener("click", () => {
@@ -117,6 +123,12 @@
         elem.classList.remove("hide");
       }
     }
+    const activeClass = active.classList;
+    const activeStore = [];
+    activeStore.push(activeClass);
+    localStorage.removeItem("all");
+    localStorage.removeItem("completed");
+    localStorage.setItem("active", JSON.stringify(activeStore));
   });
 
   completed.addEventListener("click", () => {
@@ -133,11 +145,17 @@
         elem.classList.remove("hide");
       }
     }
+    const completedClass = completed.classList;
+    const completedStore = [];
+    completedStore.push(completedClass);
+    localStorage.removeItem("all");
+    localStorage.removeItem("active");
+    localStorage.setItem("completed", JSON.stringify(completedStore));
   });
 
   clear.addEventListener("click", () => {
     active.classList.remove("checked");
-    all.classList.add("checked");
+    all.classList.remove("checked");
     completed.classList.remove("checked");
     const allCards = document.querySelectorAll(".todo_item");
     for (let card of allCards) {
@@ -149,7 +167,11 @@
   });
 
   const parseNewArr = JSON.parse(localStorage.getItem("newItems"));
+  const parseActive = JSON.parse(localStorage.getItem("active"));
+  const parseAll = JSON.parse(localStorage.getItem("all"));
+  const parseCompleted = JSON.parse(localStorage.getItem("completed"));
   window.addEventListener("load", () => {
+    
     if (parseNewArr) {
       for (let i = 0; i < parseNewArr.length; i++) {
         let newElement = document.createElement("div");
@@ -157,6 +179,41 @@
         newElement.innerHTML = parseNewArr[i];
         cards.appendChild(newElement);
         const name = newElement.closest("div").querySelector(".txt");
+        if (parseActive) {
+          active.classList.add("checked");
+          const allCards = document.querySelectorAll(".todo_item");
+          for (let elem of allCards) {
+            let txt = elem.querySelector(".txt");
+            if (txt.classList.contains("active")) {
+              elem.classList.add("hide");
+            }
+            if (!txt.classList.contains("active")) {
+              elem.classList.remove("hide");
+            }
+          }
+        }
+        if (parseAll) {
+          all.classList.add("checked");
+          const allCards = document.querySelectorAll(".todo_item");
+          for (let card of allCards) {
+            if (card.classList.contains("hide")) {
+              card.classList.remove("hide");
+            }
+          }
+        }
+        if (parseCompleted) {
+          completed.classList.add("checked");
+          const allCards = document.querySelectorAll(".todo_item");
+          for (let elem of allCards) {
+            let txt = elem.querySelector(".txt");
+            if (!txt.classList.contains("active")) {
+              elem.classList.add("hide");
+            }
+            if (txt.classList.contains("active")) {
+              elem.classList.remove("hide");
+            }
+          }
+        }
         if (name.classList.contains("active")) {
           count--;
           document.getElementById("counter").innerHTML = count;
@@ -171,3 +228,10 @@
     }
   });
 })();
+
+// const inProgres = newElement.querySelector("input");
+// const info = document.getElementById("myInput").value;
+// const newObj = {
+//   checkbox: inProgres,
+//   input: info
+// };
